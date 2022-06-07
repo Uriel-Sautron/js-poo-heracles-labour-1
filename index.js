90eaa5ff4c0f7b5fff1a5814844dea560c807295
Uriel SAUTRON <uriel.sautron@gmail.com>
2022-06-07 09:29:24 +0200

    Game done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+const Fighter = require("./src/Fighter.js");
+
+const heracles = new Fighter("🧔 Heracles", 20, 6);
+const nemean = new Fighter("🦁 Nemean Lion", 11, 13);
+let stateGame = true;
+while (stateGame) {
+  heracles.fight(nemean);
+  if (!nemean.isAlive()) stateGame = false;
+  nemean.fight(heracles);
+  if (!heracles.isAlive()) stateGame = false;
+}
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,40 @@
 /* Fighter class definition */
+class Fighter {
+  constructor(name, strength, dexterity, life = 100) {
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = life;
+  }
+
+  damage() {
+    return Math.ceil(Math.random() * (this.strength - 1) + 1);
+  }
+
+  displayFighter() {
+    console.log(`${this.name} has ${this.life}💗`);
+  }
+
+  displayRounded(enemy, damage) {
+    console.log(`${this.name} 🔫 ${enemy.name} 😵 with ${damage}damage`);
+  }
+
+  isAlive() {
+    return this.life !== 0;
+  }
+
+  fight(enemy) {
+    if (this.life !== 0) {
+      let dmg = this.damage() - enemy.dexterity;
+      dmg < 0 ? (dmg = 0) : dmg;
+      this.displayRounded(enemy, dmg);
+      enemy.life -= dmg;
+      if (enemy.life <= 0) {
+        enemy.life = 0;
+        console.log(`${this.name} 💪 ${enemy.name} 💀`);
+      }
+    }
+  }
+}
+
+module.exports = Fighter;
